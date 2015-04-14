14f98eb7968ad089a4d855c98f884087979af377
alvaro garcia <derecerca@gmail.com>
2015-04-14 19:51:00 +0200

    added canary test for speclj
    use with 'lein spec -a'

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -2,5 +2,6 @@
 (:require [speclj.core :refer :all]
           [gilded-rose.core :refer [update-quality]]))
 
-(describe "gilded rose"
-          (it "TODO - write tests later"))
+(describe "canary test"
+  (it "the framework should work"
+       (should true)))
