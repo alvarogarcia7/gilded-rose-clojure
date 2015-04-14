9951dc15e155ef2ef04fad265168b332372eac65
alvaro garcia <derecerca@gmail.com>
2015-04-14 20:11:34 +0200

    green: Once the sell by date has passed, quality degrades twice as fast

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -9,7 +9,13 @@
 (defn pass-days [n inventory]
   (nth (iterate update-quality inventory) n))
 
-(let [sulfuras [(item "Sulfuras, Hand of Ragnaros" 0 80)]]
+(let [
+        sulfuras [(item "Sulfuras, Hand of Ragnaros" 0 80)]
+        passed-item [(item "passed item" 0 10)]
+      ]
 (describe "gilded rose"
   (it "should not affect legendary items"
-       (should= sulfuras (pass-days 1 sulfuras)))))
+       (should= sulfuras (pass-days 1 sulfuras)))
+  (it "Once the sell by date has passed, quality degrades twice as fast"
+       (should= [(item "+5 Dexterity Vest" -2 8)] (pass-days 1 [(item "+5 Dexterity Vest" -1 10)])))
+          ))
