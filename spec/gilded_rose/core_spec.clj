ff834c0e48bd6a25570cae3867ab696657b32016
alvaro garcia <derecerca@gmail.com>
2015-04-14 19:55:07 +0200

    should not affect legendary items like sulfuras

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -1,7 +1,12 @@
 (ns gilded-rose.core-spec
 (:require [speclj.core :refer :all]
-          [gilded-rose.core :refer [update-quality]]))
+          [gilded-rose.core :refer [update-quality item]]))
 
 (describe "canary test"
   (it "the framework should work"
        (should true)))
+
+(let [sulfuras [(item "Sulfuras, Hand of Ragnaros" 0 80)]]
+(describe "gilded rose"
+  (it "should not affect legendary items"
+       (should (= sulfuras (update-quality sulfuras))))))
